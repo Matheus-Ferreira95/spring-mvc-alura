b2bbd84dbb81ac05db3e3af38201cc338b93b4b1
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-01-15 22:27:28 -0300

    habilitando cache

--- mvc/src/main/java/com/matheusf/mvc/repository/PedidoRepository.java
+++ mvc/src/main/java/com/matheusf/mvc/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package com.matheusf.mvc.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,7 @@ import com.matheusf.mvc.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+	@Cacheable("books")
 	@Transactional(readOnly = true)
 	List<Pedido> findByStatus(StatusPedido status, Pageable pageable);
 
